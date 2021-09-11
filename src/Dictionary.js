d3b424e495a4b09d7aa660c13fe25c7f665b82bf
catnguyen90 <catnguyen90@gmail.com>
2021-09-11 11:57:25 -0400

    create dictionary component and created form within it

---
+++ src/Dictionary.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+  const [keyword, setKeyword] = useState("");
+
+  function search(event) {
+    event.preventDefault();
+    alert(`Searching for ${keyword}`);
+  }
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+
+  return (
+    <div className="Dictionary">
+      <form className="mt-5" onSubmit={search}>
+        <input type="search" onChange={handleKeywordChange} />
+      </form>
+    </div>
+  );
+}
